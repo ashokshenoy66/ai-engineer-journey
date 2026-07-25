750604773ca5ee7d9f4df0eab777a391b78b286c
Ashok <ashokshenoy66@gmail.com>
2026-07-26 01:17:40 +0530

    Day2 -updated bmicalc added get_bmi_category function

--- week01/day2/bmicalc.py
+++ week01/day2/bmicalc.py
@@ -1,6 +1,17 @@
 def bmi(weight:float, height:float) -> float:
     return weight / (height**2)
 
-result = bmi(70,1.75)
-
+def get_bmi_category(bmi: float) -> str:
+    
+    if bmi <18.5 :
+        return "Underweight"
+    elif bmi >=18.5 and bmi <=24.9 :
+        return "Normal"
+    elif bmi >=25 and bmi <=29.9:
+        return "Overweight"
+    else:
+        return "Obese"
+    
+result = bmi(140,1.75)
 print(round(result,2))
+print(get_bmi_category(result))
